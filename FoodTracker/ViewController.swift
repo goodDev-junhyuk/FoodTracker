c0e7df1bfc7acbd2db4d88aa3e64499379724306
junhyuk <dejavukk1@gmail.com>
2020-11-08 22:58:44 +0900

    UITextField DelegatePattern. AutoLayout.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Hello iOS !!")
+        
+        nameTextField.delegate = self
     }
-
+    
+    // MARK: Action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default Label"
+        
+    }
+    
+    // MARK: UITextField Delegate Pattern.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the Keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        mealNameLabel.text = textField.text
+    }
+    
+    
+    
+    
 
 }
 

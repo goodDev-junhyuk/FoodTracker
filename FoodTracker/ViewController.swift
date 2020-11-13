733f8fc59482e36413f6c29d58cbbbdcbda87fe0
junhyuk <dejavukk1@gmail.com>
2020-11-13 16:52:20 +0900

    UIImagePickerControllerDelegate 패턴 메소드 구현(저장된 이미지 불러오기)

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -34,13 +34,42 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         // 키보드 숨기기.
         nameTextField.resignFirstResponder()
-        
+        // 사용자 사진보관함에서 미디어를 선택할 수 있는 ViewController.
+        let imagePickerController = UIImagePickerController()
+        // 찍은 사진만 선택하기.
+        imagePickerController.sourceType = .photoLibrary
+        // 사용자가 이미지를 선택할 때 알람이 나오는지.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
         
         
     }
     
+    // MARK: - UIImagePickerControllerDelegate Pattern.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+    }
     
-    // MARK: UITextField Delegate Pattern.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as?
+                UIImage else {
+                fatalError("Excepted a dictionary containing an image, but was provided the following: \(info)")
+            
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    
+    // MARK: UITextFieldDelegate Pattern.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         // Hide the Keyboard.

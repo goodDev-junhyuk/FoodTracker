30e5fa827b14732dc0a9d83711d9b2741db890ed
junhyuk <dejavukk1@gmail.com>
2020-11-24 17:29:17 +0900

    커스텀 버튼 제작 진행, @IBDesignable.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -7,41 +7,59 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+    @IBDesignable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+    @IBDesignable var starCount: Int = 5
     
     // MARK: - 이니셜라이저(Swift Language)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        
+        setupButtons()
 
     }
     
-    func ratingButtonTapped(button: UIButton) {
+    @objc func ratingButtonTapped(button: UIButton) {
         
+        print("커스텀 버튼 만들기 ..!")
         
     }
     
     // MARK: - Private Methods.
     private func setupButtons() {
         
-        // 커스텀방식으로 버튼 만들기.
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // 제약 조건
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            
+            // 커스텀방식으로 버튼 만들기.
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // 제약 조건
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+            
+            
+        }
     }
-    
-
 }
 
 
